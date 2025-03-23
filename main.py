b5fc838f21f3d0dd47a18a0475a83e1b4cb7ea5a
luca <laxertu@gmail.com>
2025-03-23 06:49:30 +0100

    potry cleo integration part 1

--- main.py
+++ main.py
@@ -1,59 +1,87 @@
-import sys
 import json
 import logging
 from json import JSONDecodeError
+
+from cleo.commands.command import Command
+from cleo.application import Application
+from cleo.helpers import argument, option
+
 from scoring_function import scoring_function
-
-try:
-    API_HOST = sys.argv[1]
-except IndexError:
-    API_HOST = 'http://127.0.0.1:8000'
-
-
 from facade import GameWrapper
 
-game_wrapper = GameWrapper(API_HOST)
-game_wrapper.game.custom_scoring_function = scoring_function
-my_game = game_wrapper.game
+def main(api_host: str, player_name: str):
+    game_wrapper = GameWrapper(api_host, player_name)
+    game_wrapper.game.custom_scoring_function = scoring_function
+    my_game = game_wrapper.game
+
+    print(f'Visit {api_host}/client/?sid={game_wrapper.session_id}')
+    input('Press [ENTER] when ready')
+
+    # Rock and roll!
+    c = game_wrapper.start()
+    my_game.custom_scoring_function = scoring_function
+
+    try:
+
+        for m in c:
+            if m.event == 'ask_move':
+                print()
+                print("Move requested")
+                try:
+                    my_game.current_player = 1
+                    my_game.board.data = json.loads(m.data)['payload']['board']
+                    game_wrapper.display_board()
+                    game_wrapper.send_move_response(my_game.player.ask_move(my_game))
+
+                except JSONDecodeError as e:
+                    logging.warning(f'wrong json: {m.data}')
+                    input('We have to restart. Press [ENTER] to close the game')
+                    game_wrapper.end()
+                    exit(0)
 
 
-print(f'Visit {API_HOST}/client/?sid={game_wrapper.session_id}')
-input('Press [ENTER] when ready')
 
-# Rock and roll!
-c = game_wrapper.start()
-my_game.custom_scoring_function = scoring_function
-
-try:
-
-    for m in c:
-        if m.event == 'ask_move':
-            print()
-            print("Move requested")
-            try:
-                my_game.current_player = 1
-                my_game.board.data = json.loads(m.data)['payload']['board']
-                game_wrapper.display_board()
-                game_wrapper.send_move_response(my_game.player.ask_move(my_game))
-
-            except JSONDecodeError as e:
-                logging.warning(f'wrong json: {m.data}')
-                input('We have to restart. Press [ENTER] to close the game')
+            elif m.event == 'game_over':
+                print()
+                print('*** AND THE WINNER IS ***')
+                print(json.loads(m.data)["payload"]["winner"])
+                print()
+                input('Press [ENTER] to close the game')
                 game_wrapper.end()
                 exit(0)
 
+    except KeyboardInterrupt:
+        print('Closing...')
+        game_wrapper.end()
+        print('Done')
 
 
-        elif m.event == 'game_over':
-            print()
-            print('*** AND THE WINNER IS ***')
-            print(json.loads(m.data)["payload"]["winner"])
-            print()
-            input('Press [ENTER] to close the game')
-            game_wrapper.end()
-            exit(0)
+class Main(Command):
+    default_player_name = "Unnamed Player"
+    name = "start"
+    description = "Start a game"
 
-except KeyboardInterrupt:
-    print('Closing...')
-    game_wrapper.end()
-    print('Done')
+    options = [
+        option(
+            "host",
+            description="Host to connect to",
+            default="https://connect4-api.onrender.com",
+            flag=False,
+        )
+    ]
+
+    def handle(self):
+        #host = self.ask(f'Connect to [Default: {Main.default_host}]', default=self.default_host)
+        host = self.option("host")
+        player_name = self.ask(f'Player name [Default: {Main.default_player_name}]', default=self.default_player_name)
+        self.line(f'Connecting to {host}')
+        main(host, player_name)
+
+
+
+
+app = Application()
+app.add(Main())
+
+if __name__ == '__main__':
+    app.run()
--- scoring_function.py
+++ scoring_function.py
@@ -1,33 +1,10 @@
 from core import Board
 
 def scoring_function(board: Board, filler_player: str, filler_opponent: str):
-
-    mask = f'{board.emptyFiller}{filler_player * 3}{board.emptyFiller}'
-    if board.substring_count(mask) > 1:
-        return 50000
-
-    mask = f'{board.emptyFiller}{filler_opponent * 3}{board.emptyFiller}'
-    if board.substring_count(mask) > 1:
-        return -50000
-
     if board.find_filler_lines_of_num(filler_player, 4) > 0:
         return 50000
 
     if board.find_filler_lines_of_num(filler_opponent, 4) > 0:
         return -50000
 
-    result = 0
-
-    result += 5 * board.find_filler_lines_of_num(filler_player, 3)
-    result += 3 * board.find_filler_lines_of_num(filler_player, 2)
-
-    result -= 5 * board.find_filler_lines_of_num(filler_opponent, 3)
-    result -= 3 * board.find_filler_lines_of_num(filler_opponent, 2)
-
-    mask = f'{board.emptyFiller}{filler_player * 3}'
-    result += board.substring_count(mask) * 2
-
-    mask = f'{board.emptyFiller}{filler_opponent * 3}'
-    result -= board.substring_count(mask) * 2
-
-    return result
+    return 0
